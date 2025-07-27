009896b7429172d993d76455e3ecebf1ec217ca3
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-27 13:31:53 +0300

    WORKING ON CALUCULATE INPUT

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+
 export const Home = () => {
+  let [input, setInput] = useState("");
+  let [initail, setInitail] = useState(1000);
+
+  function add() {
+    setInitail(initail + 500);
+  }
+
+  function del() {
+    setInitail(initail - 250);
+  }
+
   return (
     <>
-      <div className="">
-        <h1 className="text-white">hwll</h1>
+      <div className="mt-10 flex justify-center">
+        <h1 className="text-white text-2xl">{initail}</h1>
+        <button
+          className="p-3 rounded-2xl bg-amber-50 text-black"
+          onClick={add}
+        >
+          +500
+        </button>
+        <button
+          className="p-3 rounded-2xl bg-amber-50 text-black"
+          onClick={del}
+        >
+          -250
+        </button>
+
+        <input
+          type="text"
+          className="p-3 bg-amber-50 text-black rounded-2xl"
+          placeholder="Type.."
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          className="p-3 rounded-2xl bg-amber-50 text-black"
+          onClick={del}
+        >
+          Done
+        </button>
       </div>
     </>
   );
